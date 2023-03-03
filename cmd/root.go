ca033189fc2e53babb7f83ad5981b7d6c7792720
braunbearded <braunbearded1@gmail.com>
2023-03-04 00:37:54 +0100

    Add option for custom headers

--- cmd/root.go
+++ cmd/root.go
@@ -41,6 +41,7 @@ type skweezConf struct {
 	jsonOutput bool
 	targets    []string
 	urlFilter  []*regexp.Regexp
+	header     []string
 }
 
 var validWordRegex = regexp.MustCompile(`^[a-zA-Z0-9]+.*[a-zA-Z0-9]$`)
@@ -71,6 +72,8 @@ crawl websites to generate word lists.`,
 		paramNoFilter, err := cmd.LocalFlags().GetBool("no-filter")
 		handleErr(err, false)
 		paramJsonOutput, err := cmd.LocalFlags().GetBool("json")
+		handleErr(err, false)
+		paramHeader, err := cmd.LocalFlags().GetStringSlice("header")
 		handleErr(err, false)
 		// sanitize scope param
 		sanitizedScope := []string{}
@@ -106,6 +109,7 @@ crawl websites to generate word lists.`,
 			noFilter:   paramNoFilter,
 			jsonOutput: paramJsonOutput,
 			targets:    preparedTargets,
+			header:     paramHeader,
 		}
 		run(config)
 	},
@@ -125,6 +129,7 @@ func init() {
 	rootCmd.Flags().Bool("no-filter", false, "Do not filter out strings that don't match the regex to check if it looks like a valid word (starts and ends with alphanumeric letter, anything else in between). Also ignores --min-word-length and --max-word-length")
 	rootCmd.Flags().Bool("json", false, "Write words + counts in a json file. Requires --output/-o")
 	rootCmd.Flags().Bool("debug", false, "Enable Debug output")
+	rootCmd.Flags().StringSlice("header", []string{}, "Additional header")
 }
 
 func handleErr(err error, critical bool) {
@@ -167,6 +172,14 @@ func registerCallbacks(collector *colly.Collector, config *skweezConf, cache *ma
 	})
 
 	collector.OnRequest(func(r *colly.Request) {
+		if len(config.header) > 0 {
+			for _, header := range config.header {
+				var headerSplit = strings.SplitN(header, ":", 2)
+				if len(headerSplit) > 1 {
+					r.Headers.Set(headerSplit[0], headerSplit[1])
+				}
+			}
+		}
 		if config.debug {
 			logger.Println("Visiting", r.URL)
 		}
